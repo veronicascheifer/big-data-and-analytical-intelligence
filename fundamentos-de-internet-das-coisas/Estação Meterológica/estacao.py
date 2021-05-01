9e1a2aac4b8811b6a2f8225b9a74bb520a4cba12
Verônica <55854560+veronicascheifer@users.noreply.github.com>
2021-04-30 23:54:07 -0300

    add http post

--- fundamentos-de-internet-das-coisas/Estação Meterológica/estacao.py
+++ fundamentos-de-internet-das-coisas/Estação Meterológica/estacao.py
@@ -1,6 +1,23 @@
 import dht
 import machine
 import time
+import network
+import urequests
+import ujson
+
+def conecta(ssid, senha):
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(ssid, senha)
+    return station
+
+print("Conectando...")
+station = conecta("nome da rede", "senha")
+if not station.isconnected():
+    print("Nao conectado!")
+else:
+    print("Conectado!")
+
 
 r = machine.Pin(2, machine.Pin.OUT)
 d = dht.DHT11(machine.Pin(4))
@@ -9,17 +26,20 @@ x = 0
 
 while x < 10:
     d.measure()
-    temperatura = d.temperature()
-    umidade = d.humidity()
-    
-    print("Temp={}   Umid={}".format(d.temperature(), d.humidity()))
-    
-    if temperatura > 31 or umidade > 70:
+    temperatura = ("{}".format(d.temperature()))
+    umidade = ("{}".format(d.humidity()))
+
+    if int(temperatura) > 31 or int(umidade) > 70:
         r.value(1)
     else:
         r.value(0)
-        
-    print("Temp={}   Umid={}".format(d.temperature(), d.humidity()))
+            
+    dados = {'field1': temperatura, 'field2': umidade}
+    request = urequests.post('https://api.thingspeak.com/update?api_key=J4NKP906UTS1Z58Q',
+        json = dados,
+        headers = {'content-type': 'application/json'})
     
-    time.sleep(1)
-    x += 1
+    request.close()   
+    
+    time.sleep(15)
+    x += 1 
