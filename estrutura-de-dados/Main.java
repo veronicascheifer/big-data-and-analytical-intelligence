22195b32649c4b3e71920c13f1406c6403f9bdfb
Verônica <55854560+veronicascheifer@users.noreply.github.com>
2021-07-04 22:48:49 -0300

    Add files via upload

---
+++ estrutura-de-dados/Main.java
@@ -0,0 +1,30 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		System.out.println("Lista Encadeada");
+		
+		ListaEncadeada lista = new ListaEncadeada();
+		
+		lista.insereOrdenado("arq1.txt", 1);
+		lista.insereOrdenado("arq3.txt", 10);
+		lista.insereOrdenado("arq2.txt", 5);
+		lista.insereOrdenado("arq4.txt", 3);
+		
+		lista.imprimeArquivos();
+		
+        ArvoreBinariaBusca arvore = new ArvoreBinariaBusca();
+        arvore.inserir("bola", "arq1.txt");
+        arvore.inserir("casa", "arq1.txt");
+        arvore.inserir("dado", "arq1.txt");
+        arvore.inserir("bola", "arq1.txt");
+        arvore.inserir("casa", "arq1.txt");
+        arvore.inserir("dado", "arq2.txt");
+        arvore.inserir("bola", "arq2.txt");
+        arvore.inserir("arvore", "arq2.txt");
+        arvore.inserir("arvore", "arq2.txt");
+        arvore.inserir("arvore", "arq2.txt");
+        arvore.inserir("arvore", "arq2.txt");
+        arvore.inOrder();
+	}
+}
