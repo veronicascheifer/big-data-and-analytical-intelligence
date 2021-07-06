9a8496650b4c1f3b82044e190fdfa1213ef5829b
Verônica <55854560+veronicascheifer@users.noreply.github.com>
2021-07-05 22:10:40 -0300

    add buscador

--- estrutura-de-dados/Main.java
+++ estrutura-de-dados/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main
 {
 	public static void main(String[] args) {
@@ -11,8 +13,6 @@ public class Main
 		lista.insereOrdenado("arq2.txt", 5);
 		lista.insereOrdenado("arq4.txt", 3);
 		
-		lista.imprimeArquivos();
-		
         ArvoreBinariaBusca arvore = new ArvoreBinariaBusca();
         arvore.inserir("bola", "arq1.txt");
         arvore.inserir("casa", "arq1.txt");
@@ -25,6 +25,14 @@ public class Main
         arvore.inserir("arvore", "arq2.txt");
         arvore.inserir("arvore", "arq2.txt");
         arvore.inserir("arvore", "arq2.txt");
-        arvore.inOrder();
+
+        //Simulacao do input do usuario
+        ArrayList<String> termos = new ArrayList<String>();
+        termos.add("casa");
+        termos.add("bola");
+        termos.add("dado");
+        
+        Buscador buscador = new Buscador();
+        buscador.buscaTermos(termos, lista, arvore);
 	}
 }
